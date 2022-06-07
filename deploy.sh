4e78806ca9cfc771a30a8fd4dd6d62e5b09087fa
leicaflorian <florian.leica@gmail.com>
2022-06-08 00:37:01 +0200

    deploy

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:Florian-Bool-65/workshop_js.git main:gh-pages
+cd -
