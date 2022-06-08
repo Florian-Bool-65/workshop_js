9267498de72b504fa08fb0c1918e0c313ae03fee
Domenico Monaco <monaco.d23@gmail.com>
2022-06-08 18:26:19 +0200

    refactoring code

--- js/domEventsHandler.js
+++ js/domEventsHandler.js
@@ -3,3 +3,20 @@
 // Gestisce tutti gli eventi del DOM
 //
 ////////////////////////////////////////////////////////////
+
+
+const likebuttonslist = document.querySelectorAll(".selected-like-button");
+
+likebuttonslist.forEach((element) => {
+  element.addEventListener("click", function () {
+    let datapostid = element.getAttribute("data-postid");
+
+    let likespost = document.getElementById("like-counter-" + datapostid);
+
+    let currentlike = parseInt(likespost.getAttribute("data-likepost"));
+    currentlike += 1;
+
+    likespost.setAttribute("data-likepost", currentlike);
+    likespost.innerHTML = currentlike;
+  });
+});
--- js/main.js
+++ js/main.js
@@ -26,18 +26,3 @@ listaPosts.forEach(function (post) {
   }
 });
 
-const likebuttonslist = document.querySelectorAll(".selected-like-button");
-
-likebuttonslist.forEach((element) => {
-  element.addEventListener("click", function () {
-    let datapostid = element.getAttribute("data-postid");
-
-    let likespost = document.getElementById("like-counter-" + datapostid);
-
-    let currentlike = parseInt(likespost.getAttribute("data-likepost"));
-    currentlike += 1;
-
-    likespost.setAttribute("data-likepost", currentlike);
-    likespost.innerHTML = currentlike;
-  });
-});
